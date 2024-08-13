4001c9bb312140a1baa73fdf7f6b791c71410975
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-08-12 21:54:30 -0400

    se termino la funcion para contar los caracteres se una cadena de texto, se hizo para una cadena de texto almacenada en el codigo y para una cadena de texto que pueda ingresar el usuario(procurar que sea sin espacios ~ mejorar la parte de 1 - 10 como implementar un ciclo)

--- laboratorio_0.cpp
+++ laboratorio_0.cpp
@@ -46,18 +46,24 @@ void imprimir_par_impar(int numero)
     }
 }
 
+//la funcion cuenta todos los espacios que tenga una cadena de texto
 int longitud(std::string cadena_de_texto)
 {
-    int longitud = 0;
     //procesamiento de la cadena de texto
 
-    //arreglar bucle
-    /*
-    for char in cadena_de_texto;
-        longitud = longitud + 1
 
-    return longitud_de_la_cadena
-    */
+    //en c++ los string tambien funcionan como arrays hmm
+    int longitud = 0;
+    //for (inicialización; condición; actualización)
+    //se iterara cada caracter mientras que el caracter no sea \0 que es el final de la cadena
+    for (int i = 0; cadena_de_texto[i] != '\0'; ++i)
+    {
+        //se sumara 1 a la longitud por cada caracter en la cadena
+        longitud = longitud + 1;
+    }
+
+    return longitud;
+    
 }
 
 int main()
@@ -91,23 +97,32 @@ int main()
     imprimir_par_impar(10);
     imprimir_par_impar(user_input);
 
-    //funcion leer string ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    //funcion leer cadena ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     std::string mi_cadena = "Hola Mundo";
-    std::cout << "Cadena de ejemplo --> ""(Hola Mundo)""\n";
 
-    //longitud_cadena = longitud(mi_cadena);
+    //se pide la cadena sin espacios porque si se toma un espacio la variable guarda la cadena hasta el espacio
+    //escribe ~ "Hola Mundo", se guarda ~ "Hola"
+    std::cout << "\n\nCadena almacenada en el codigo --> " << mi_cadena << "\n\nIngrese una cadena de texto\nSIN ESPACIOS\n";
 
+    //se guarda la cadena que ingrese el usuario
+    std::string user_input_cadena = "0";
+    std::cin >> user_input_cadena;
 
-    /*
-    //ambas funciones devuelven el mismo numero
+    std::cout << "\n\nCadena del usuario --> " << user_input_cadena << "\n";
+    
+    /*//ambas funciones devuelven el mismo numero
     int longitud1 = mi_cadena.size();
     std::cout << "El tamaño de la cadena es de " << longitud1 << " caracteres (.size())\n\n";
 
     int longitud2 = mi_cadena.length();
     std::cout << "El tamaño de la cadena es de " << longitud2 << " caracteres (.length())";*/
 
-    return 0;
+    int longitud_cadena1 = longitud(mi_cadena);
+    int longitud_cadena2 = longitud(user_input_cadena);
 
+    std::cout << "\n\nlongitud: " << longitud_cadena1 << "caracteres ~ Cadena: " << mi_cadena << "\n";
+    std::cout << "\nlongitud: " << longitud_cadena2 << "caracteres ~ Cadena: " << user_input_cadena << "\n";
+
+    return 0;
 }
