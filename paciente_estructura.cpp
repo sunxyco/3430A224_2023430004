6a3f8b7e7114041bf03ffa4ac565713c4871af5a
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-08-27 09:21:04 -0400

    creacion de 3 estructuras pacientes y se imprime por teminal que funciona bien

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -9,3 +9,27 @@ struct Paciente
     double altura;
 };
 
+int main () {
+
+    Paciente paciente1;
+    paciente1.nombre = "Martin";
+    paciente1.edad = 11;
+    paciente1.altura = 1.50;
+
+    Paciente paciente2;
+    paciente2.nombre = "Carlota";
+    paciente2.edad = 23;
+    paciente2.altura = 1.70;
+
+    Paciente paciente3;
+    paciente3.nombre = "Gonzalo";
+    paciente3.edad = 45;
+    paciente3.altura = 1.90;
+
+
+    cout << "Pacientes\n\nPaciente 1\n" << "Nombre:" << paciente1.nombre << " Edad: " << paciente1.edad << " altura: "<< paciente1.altura <<"\n";
+    cout << "\nPaciente 2\n" << "Nombre:" << paciente2.nombre << " Edad: " << paciente2.edad << " altura: "<< paciente2.altura <<"\n";
+    cout << "\nPaciente 3\n" << "Nombre:" << paciente3.nombre << " Edad: " << paciente3.edad << " altura: "<< paciente3.altura <<"\n";
+
+    return 0;
+}
