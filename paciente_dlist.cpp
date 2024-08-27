bf533a63de83eeac76c8cac264984574b383b7fc
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-08-27 09:53:12 -0400

    se le agrego un puntero a la estructura con el fin de hacer una lista dinamica

---
+++ paciente_dlist.cpp
@@ -0,0 +1,30 @@
+/*Escriba programa en C++ que permita almacenar la información de pacientes en
+una estructura para trabajar con una lista dinámica. Aquí no se puede utilizar la librería
+list y debe realizar la actualización de la estructura, la inclusión de las funciones para
+agregar, imprimir y eliminar. Dentro de la función main debe incorporar los ejemplos de
+pacientes y dejar en evidencia todas las funciones desarrolladas*/
+
+/*Escriba un programa en C++ que permita almacenar la información de los pacientes
+(estructura) en un arreglo. Dentro de la función main debe incorporar los ejemplos de
+“pacientes” utilizados anteriormente, almacenarlos en el arreglo y entregar la
+información de cada uno de ellos por la salida estándar.*/
+
+
+#include <iostream>
+using namespace std;
+
+//la estructura de tipo paciente
+struct Paciente
+{
+    string nombre;
+    int edad;
+    double altura;
+
+    Paciente* siguiente;
+};
+
+int main () {
+
+
+   return 0;
+}
