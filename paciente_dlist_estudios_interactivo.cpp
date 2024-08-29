0e5a0500aa94b2500100183c230a5cfbe1079e1a
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-08-28 20:10:30 -0400

    esta es la idea base de la interaccion con el usuario

---
+++ paciente_dlist_estudios_interactivo.cpp
@@ -0,0 +1,23 @@
+/*Escriba un programa en C++ que permita trabajar con la gestión de pacientes a través
+de opciones interactivas. Es de interés poder incorporar un menú que permita a un
+usuario navegar por las opciones que existen y utilizar todas las funcionalidades
+(funciones desarrolladas). Además se debe sumar la posibilidad de realizar la carga
+de información desde un archivo de texto plano o CSV.
+*/
+
+
+//menu que permita utilizar todas las funciones desarrolladas
+//cargar informacion desde un texto plano ~ csv
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ingreso_usuario;
+
+    cout << "Hello, World!\n\n1~ Agregar Paciente\n2~ Eliminar Paciente\n3~ Imprimir Pacientes Actuales\n4~ Imprimir promedio de Edad\n4~ Imprimir promedio de Peso\n6~ Imprimir IMC pacientes\n\n~~";
+    cin >> ingreso_usuario;
+
+    cout << "\n\n\n~~" << ingreso_usuario;
+}
