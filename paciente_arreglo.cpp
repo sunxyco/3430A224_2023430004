9c4ccb3eb7c8f30e93f00df7bb75a30149b49cda
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-08-29 18:35:37 -0400

    codigo modificado y comentado

--- paciente_arreglo.cpp
+++ paciente_arreglo.cpp
@@ -1,9 +1,3 @@
-/*Escriba un programa en C++ que permita almacenar la información de los pacientes
-(estructura) en un arreglo. Dentro de la función main debe incorporar los ejemplos de
-“pacientes” utilizados anteriormente, almacenarlos en el arreglo y entregar la
-información de cada uno de ellos por la salida estándar.*/
-
-
 #include <iostream>
 using namespace std;
 
@@ -11,33 +5,40 @@ using namespace std;
 struct Paciente
 {
     string nombre;
+    int peso;
     int edad;
     double altura;
 };
 
 int main () {
 
-    //hacer arreglo qui
+    //se crea un arreglo de 3 instancias de tipo Paciente
     Paciente mis_pacientes[3];
 
+    //se modifica el arreglo con los datos de nuestros pacientes
     mis_pacientes[0].nombre = "Martin";
+    mis_pacientes[0].peso = 43;
     mis_pacientes[0].edad = 11;
     mis_pacientes[0].altura = 1.50;
 
     mis_pacientes[1].nombre = "Carlota";
+    mis_pacientes[1].peso = 65;
     mis_pacientes[1].edad = 23;
     mis_pacientes[1].altura = 1.70;
 
     mis_pacientes[2].nombre = "Gonzalo";
+    mis_pacientes[2].peso = 140;
     mis_pacientes[2].edad = 45;
     mis_pacientes[2].altura = 1.90;
 
 
+    //se imprimen los datos de nuestros pacientes
     for (int i = 0; i < 3; ++i) {
         cout << "Paciente " << i+1 << "\n";
         cout << "Nombre: " << mis_pacientes[i].nombre << " ";
+        cout << "Peso: " << mis_pacientes[i].peso << "kg ";
         cout << "Edad: " << mis_pacientes[i].edad << " ";
-        cout << "Altura: " << mis_pacientes[i].altura << " ";
+        cout << "Altura: " << mis_pacientes[i].altura << "m ";
         cout << "\n\n";
     }
 
--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -5,31 +5,36 @@ using namespace std;
 struct Paciente
 {
     string nombre;
+    int peso;
     int edad;
     double altura;
 };
 
 int main () {
 
+    //se crean 3 pacientes segun la estructura
     Paciente paciente1;
     paciente1.nombre = "Martin";
+    paciente1.peso = 43;
     paciente1.edad = 11;
     paciente1.altura = 1.50;
 
     Paciente paciente2;
     paciente2.nombre = "Carlota";
+    paciente2.peso = 65;
     paciente2.edad = 23;
     paciente2.altura = 1.70;
 
     Paciente paciente3;
     paciente3.nombre = "Gonzalo";
+    paciente3.peso = 140;
     paciente3.edad = 45;
     paciente3.altura = 1.90;
 
-
-    cout << "Pacientes\n\nPaciente 1\n" << "Nombre:" << paciente1.nombre << " Edad: " << paciente1.edad << " altura: "<< paciente1.altura <<"\n";
-    cout << "\nPaciente 2\n" << "Nombre:" << paciente2.nombre << " Edad: " << paciente2.edad << " altura: "<< paciente2.altura <<"\n";
-    cout << "\nPaciente 3\n" << "Nombre:" << paciente3.nombre << " Edad: " << paciente3.edad << " altura: "<< paciente3.altura <<"\n";
+    //se imprimen los 3 pacientes
+    cout << "Pacientes\n\nPaciente 1\n" << "Nombre:" << paciente1.nombre << " Edad: " << paciente1.edad << " Peso: " << paciente1.peso << "kg Altura: "<< paciente1.altura <<"m\n";
+    cout << "\nPaciente 2\n" << "Nombre:" << paciente2.nombre << " Edad: " << paciente2.edad << " Peso: " << paciente2.peso << "kg Altura: "<< paciente2.altura <<"m\n";
+    cout << "\nPaciente 3\n" << "Nombre:" << paciente3.nombre << " Edad: " << paciente3.edad << " Peso: " << paciente3.peso << "kg Altura: "<< paciente3.altura <<"m\n";
 
     return 0;
 }
