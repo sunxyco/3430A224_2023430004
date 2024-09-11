ab39ead013211f9fbb845b4d149ea94719402bd5
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-09-11 19:13:09 -0300

    pila ~ hecho -creacion de la pila con numero dinamico de espacions -push ~falta pop y menu interaccion con el usuario

--- imorales_lab3_ejercicio1.cpp
+++ imorales_lab3_ejercicio1.cpp
@@ -1,71 +1,52 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-struct Pila{
+struct Pila {
     int maximo;
     int tope;
-    int mi_array[maximo];
-    bool band_vacio;
-    bool band_lleno;
+    int* valores; // Usamos un puntero para manejar el array dinámicamente
+
+    // Constructor para inicializar la pila - usado para cuando se crea la pila
+    Pila(int max) : maximo(max), tope(0) {
+        valores = new int[maximo]; // Asignamos memoria para el array
+    }
+
+    // Método para agregar un elemento a la pila
+    void push(int valor) {
+        if (tope < maximo) {
+            valores[tope] = valor; // Agrega el elemento y aumenta el tope
+            tope = tope + 1;
+        } else {
+            cout << "Pila llena" << "\n\n";
+        }
+    }
+
+    // Método para mostrar los elementos de la pila
+    void mostrar() {
+        for (int i = 0; i < tope; i++) {
+            cout << valores[i] << " ";
+        }
+        cout << "\n\n";
+    }
 };
 
-void crear_pila(int max){
-    //crear array con un numeor {maximo} de espacios
-}
+int main() {
+    cout << "Hola mundo" << "\n\n";
 
-void push(int dato){
-    //añadir al array
-}
+    // Crear pila de 4 espacios
+    Pila mi_pila(4);
+    cout << "Se creó la pila" << "\n\n";
 
-void pop(int dato){
-    //recorrer el array y remover el dato - reposicionar los demas datos
-}
+    // Agregar elementos a la pila
+    mi_pila.push(1);
+    mi_pila.push(2);
+    mi_pila.push(3);
+    mi_pila.push(4);
+    mi_pila.push(5); // Intento de agregar un quinto elemento
 
-//chequea si la pila está vacia - ve si el ultimo elemento agregado esta en la posicion 0
-void pila_vacia(tope, band){
-    si tope == 0 {
-        hacer band <- verdadero
-    sino 
-        hacer band <- falso
-    }
-}
+    // Mostrar los elementos de la pila
+    mi_pila.mostrar();
 
-//chequea si la pila esta llena
-void pila_llena(tope, max, band){
-    si tope == max
-        hacer band <- verdadero
-    sino
-        hacer band <- falso
-}
-
-// PILA y TOPE por referencia.
-void Push(PILA, TOPE, MAX, DATO)
-    Llamar a Pila_llena(TOPE, MAX, BAND)
-    Si (BAND = VERDADERO) entonces
-        Escribir "Desbordamiento, Pila llena"
-    Sino
-        Hacer TOPE <- TOPE + 1
-        Hacer PILA[TOPE] <- DATO
-
-int main(){
-    cout << "hola mundo"
-
-
-    //crear pila de 8 espacios
-    crear_pila(8);
-    push(4);
-    push(2);
-    push(1);
-    push(4);
-    push(3);
-
-    //recorrer array de los datos
-    ver_pila();
-
-    //pop
-    remover(1);
-
-    ver_pila();
-
-    return 0
+    return 0;
 }
