a78ae0fea27d05c2ec983efa6ac9f8dc59f3265a
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-09-11 23:17:19 -0300

    apuntes basicos

--- imorales_lab3_ejercicio2.cpp
+++ imorales_lab3_ejercicio2.cpp
@@ -4,4 +4,24 @@ using namespace std;
 
 int main(){
     cout << "Holamundo";
+
+    //m -> referente a cantidad de pilas
+
+    //puerto almacena m cantidad de pilas
+
+    //una pila almacena n cantidad de contenedores
+
+    //puerto -> array de pilas de contenedores
+
+
+    //leer por terminal n y m
+
+    //especie de matriz
+
+    //mostrar por pantalla cada movimiento realizado
+
+    int n = 4;
+    int m = 3;
+
+    
 }
