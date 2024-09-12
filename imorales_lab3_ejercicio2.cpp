6f3e7670e1625b5c3aa220dc707073babb71de31
Ignacio Morales <imorales23@alumnos.utalca.cl>
2024-09-12 18:15:04 -0300

    el usuario puede ingresar la cantidad de pilas y la capacidad de las pilas, seguido se generan aleatoriamente los contenedores y se llena automaticamente el puerto

--- imorales_lab3_ejercicio2.cpp
+++ imorales_lab3_ejercicio2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <random>
+#include <ctime>
 using namespace std;
 
 struct Pila {
@@ -7,7 +9,7 @@ struct Pila {
     int tope;
     string* contenedores; // Usamos un puntero para manejar el array dinámicamente
 
-    // Constructor para inicializar la pila - usado para cuando se crea la pila
+    // Constructor para inicializar la pila
     Pila(int max) : maximo(max), tope(0) {
         contenedores = new string[maximo]; // Asignamos memoria para el array
     }
@@ -22,7 +24,7 @@ struct Pila {
         }
     }
 
-    // Método para eliminar el último elemento de la pila ~ se reduce el tamaño de el array en 1 y se deja el ultimo elemento afuera
+    // Método para eliminar el último elemento de la pila
     void pop() {
         if (tope == 0) { // Verificar si la pila está vacía
             cout << "No hay elementos para eliminar" << "\n\n";
@@ -38,15 +40,15 @@ struct Pila {
         for (int i = 0; i < tope; i++) {
             cout << contenedores[i];
 
-            if (tope != i + 1){
+            if (tope != i + 1) {
                 cout << ", ";
             }
         }
-        cout << "]\n\n";
+        cout << "]\n";
     }
 };
 
-//referente a un array de pilas
+// Estructura referente a un array de pilas
 struct Puerto {
     int espacios_disponibles;  // Cantidad de pilas en el puerto
     Pila** bloques_pilas;      // Puntero a un array dinámico de punteros a Pilas
@@ -61,13 +63,32 @@ struct Puerto {
 
     // Método para mostrar el contenido de todas las pilas en el puerto
     void mostrar_puerto() {
+        cout << "\n~~Contenedores actuales en el Puerto~~\n\n";
         for (int i = 0; i < espacios_disponibles; i++) {
             cout << "Pila " << i + 1 << ": ";
             bloques_pilas[i]->mostrar();
         }
+        cour << "\n\n"
     }
 
-    //metodo para eliminar un contenedor en especifico
+    // Método para agregar elementos aleatorios
+    void agregar_elementos_aleatorios() {
+        // Inicializar generador aleatorio
+        srand(static_cast<unsigned int>(time(0))); // Semilla para el generador aleatorio
+
+        for (int i = 0; i < espacios_disponibles; i++) {
+            int num_elementos = rand() % (bloques_pilas[i]->maximo + 1); // Número aleatorio de elementos a agregar
+            for (int j = 0; j < num_elementos; j++) {
+                // Generar un string en el formato "x/EMPy"
+                int x = rand() % 4 + 1; // Número aleatorio entre 1 y 4
+                int y = rand() % 3 + 1; // Número aleatorio entre 1 y 3
+                string contenedor = to_string(x) + "/EMP" + to_string(y);
+                bloques_pilas[i]->push(contenedor);
+            }
+        }
+    }
+
+    // Método para eliminar un contenedor en específico
     void eliminar_contenedor(string contenedor_a_eliminar) {
         for (int bloque = 0; bloque < espacios_disponibles; bloque++) {
             cout << "Buscando en pila " << bloque + 1 << "\n";
@@ -117,51 +138,39 @@ struct Puerto {
     }
 };
 
-int main(){
-    cout << "Holamundo" << "\n\n";
+int main() {
+    string ingreso_num_pilas;
+    string ingreso_num_capacidad;
 
-    //m -> referente a cantidad de pilas
+    cout << "Ingrese numero de Pilas\n>";
+    cin >> ingreso_num_pilas; // Cambié esto para que lea la entrada
+    cout << "Ingrese Capacidad de las Pilas\n> ";
+    cin >> ingreso_num_capacidad; // Cambié esto para que lea la entrada
 
-    //puerto almacena m cantidad de pilas
+    // n
+    int num_pilas = stoi(ingreso_num_pilas);
+    // m
+    int capacidad_pilas = stoi(ingreso_num_capacidad);
 
-    //una pila almacena n cantidad de contenedores
-
-    //puerto -> array de pilas de contenedores
-
-
-    //leer por terminal n y m
-
-    //especie de matriz
-
-    //mostrar por pantalla cada movimiento realizado
-
-    // Crear un puerto con 3 pilas, donde cada pila puede almacenar hasta 5 elementos
-
-    //n
-    int num_pilas = 4;
-    //m
-    int capacidad_pilas = 5;
     Puerto mi_puerto(num_pilas, capacidad_pilas);
 
     // Agregar elementos a las pilas
-    mi_puerto.bloques_pilas[0]->push("1/EMP1");
-    mi_puerto.bloques_pilas[0]->push("2/EMP1");
-    mi_puerto.bloques_pilas[1]->push("1/EMP2");
-    mi_puerto.bloques_pilas[2]->push("1/EMP3");
-    mi_puerto.bloques_pilas[2]->push("2/EMP3");
-    mi_puerto.bloques_pilas[0]->push("3/EMP1");
+    mi_puerto.agregar_elementos_aleatorios();
 
     // Mostrar el contenido del puerto
     mi_puerto.mostrar_puerto();
 
+    /*
     cout << "Buscar contenedor ~ 1/EMP1\n";
     mi_puerto.eliminar_contenedor("1/EMP1");
 
     mi_puerto.mostrar_puerto();
 
     cout << "\n\nBuscar contenedor ~ 2/EMP3\n";
-    mi_puerto.eliminar_contenedor("2/EMP1");
+    mi_puerto.eliminar_contenedor("2/EMP3");
 
     mi_puerto.mostrar_puerto();
+    */
+
     return 0;
 }
